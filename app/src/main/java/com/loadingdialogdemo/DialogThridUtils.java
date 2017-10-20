7455f14c3c300237000f458ad27257b2f5d1c26a
zhangqi960110 <csu_zhangqi@163.com>
2017-10-20 09:12:49 +0800

    Update DialogThridUtils.java

--- app/src/main/java/com/loadingdialogdemo/DialogThridUtils.java
+++ app/src/main/java/com/loadingdialogdemo/DialogThridUtils.java
@@ -13,12 +13,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-/**
- * @author ：程序员小冰
- * @新浪微博 ：http://weibo.com/mcxiaobing
- * @GitHub: https://github.com/QQ986945193
- * @CSDN博客: http://blog.csdn.net/qq_21376985
- */
 public class DialogThridUtils {
     /**
      * 显示Dialog
