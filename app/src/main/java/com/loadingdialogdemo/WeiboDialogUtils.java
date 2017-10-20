dc584959f9f16d9b42956990ebc1be7622e826d3
zhangqi960110 <csu_zhangqi@163.com>
2017-10-20 09:13:14 +0800

    Update WeiboDialogUtils.java

--- app/src/main/java/com/loadingdialogdemo/WeiboDialogUtils.java
+++ app/src/main/java/com/loadingdialogdemo/WeiboDialogUtils.java
@@ -10,13 +10,6 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-/**
- * @author ：程序员小冰
- * @新浪微博 ：http://weibo.com/mcxiaobing
- * @GitHub: https://github.com/QQ986945193
- * @CSDN博客: http://blog.csdn.net/qq_21376985
- */
-
 public class WeiboDialogUtils {
 
     public static Dialog createLoadingDialog(Context context, String msg) {
